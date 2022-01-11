cb51e7c304cdbcd4323d6c7cda134dc0770a3740
Aline-G <65853969+Aline-G@users.noreply.github.com>
2022-01-11 15:03:00 +0100

    Creation of data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO mission (id,date,description,name)
+VALUES (1,'2022-01-06','ramasser tous les sacs','Vider les poubelles'),
+       (2,'2022-01-06','Charmer les clients','Charme');
+
+INSERT INTO line_bill (id,amount,country,date,justificatif,tva,tva_percent,is_validated)
+VALUES (1,500, 'France','2022-01-06',null,12,2.4,FALSE );
+
+
