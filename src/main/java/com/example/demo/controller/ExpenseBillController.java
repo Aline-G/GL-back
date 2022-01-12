fa4d6e24f1991a4293ad6b80fff43430d029183f
guillaume-besse <38402818+guillaume-besse@users.noreply.github.com>
2022-01-12 17:00:47 +0100

    delete expenseBill Add ExpenseBillId in line bill's attributes Validation of lineBill and to validate ExpenseBill when all lines are validated

--- src/main/java/com/example/demo/controller/ExpenseBillController.java
+++ src/main/java/com/example/demo/controller/ExpenseBillController.java
@@ -3,22 +3,20 @@ package com.example.demo.controller;
 
 import com.example.demo.exception.DateException;
 import com.example.demo.exception.ExpenseBillException;
-import com.example.demo.exception.LineBillException;
-import com.example.demo.exception.MissionException;
 import com.example.demo.service.DateService;
 import com.example.demo.service.ExpenseBillService;
 import com.example.demo.vo.BillStates;
 import com.example.demo.vo.ExpenseBill;
-import com.example.demo.vo.LineBill;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.ArrayList;
 import java.util.List;
+
 
 @Getter
 @RequestMapping("/expensebill")
@@ -47,6 +45,18 @@ public class ExpenseBillController {
         return e;
     }
 
+    @GetMapping("/delete")
+    public HttpStatus deleteExpenseBill(@RequestParam int id) throws ExpenseBillException {
+        return this.expenseBillService.deleteExpenseBill(id);
+    }
+
+    @GetMapping("/list")
+    public List<ExpenseBill> getExpenseBillList() {
+        return this.expenseBillService.getExpenseBillList();
+    }
+
+    //TODO validation de toute la note de frais
 
 
+    // TODO quand utilisateur sera codé faire des fonction de get en fonction de UserID passe en parametre
 }
--- src/main/java/com/example/demo/service/ExpenseBillService.java
+++ src/main/java/com/example/demo/service/ExpenseBillService.java
@@ -2,12 +2,17 @@ package com.example.demo.service;
 
 import com.example.demo.exception.DateException;
 import com.example.demo.exception.ExpenseBillException;
-import com.example.demo.exception.LineBillException;
 import com.example.demo.repository.ExpenseBillRepository;
+import com.example.demo.vo.BillStates;
 import com.example.demo.vo.ExpenseBill;
+import com.example.demo.vo.LineBill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class ExpenseBillService {
@@ -28,5 +33,59 @@ public class ExpenseBillService {
         return this.expenseBillRepository.save(expenseBill);
     }
 
+    public HttpStatus deleteExpenseBill(int id) throws ExpenseBillException {
+        //Check existence of this id
+        if(!expenseBillRepository.existsById(id)){
+            throw new ExpenseBillException("Id doesn't exist", HttpStatus.BAD_REQUEST);
+        }
+        this.expenseBillRepository.delete(this.expenseBillRepository.findById(id));
+        return HttpStatus.OK;
+    }
+
+    public void addLineBill(LineBill lineBill, int idExpenseBill) throws ExpenseBillException {
+        if(!expenseBillRepository.existsById(idExpenseBill)){
+            throw new ExpenseBillException("Id doesn't exist", HttpStatus.BAD_REQUEST);
+        }
+
+        ExpenseBill expenseBill = expenseBillRepository.findById(idExpenseBill);
+        List<LineBill> list = expenseBill.getListLineBill();
+        list.add(lineBill);
+        expenseBill.setListLineBill(list);
+        expenseBill.setAmount(expenseBill.getAmount()+lineBill.getAmount());
+        expenseBillRepository.save(expenseBill);
+    }
+
+    public List<ExpenseBill> getExpenseBillList() {
+        return StreamSupport.stream(this.expenseBillRepository.findAll().spliterator(), false).collect(Collectors.toList());
+    }
+
+
+    public void existsById(int idExpenseBill) throws ExpenseBillException {
+        if(!this.expenseBillRepository.existsById(idExpenseBill)){
+            throw new ExpenseBillException("This Expense Bill does not exists",HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // TODO check aussi que les avances sont valide
+    public boolean allValidated(int expenseBillId) throws ExpenseBillException {
+        existsById(expenseBillId);
+        ExpenseBill expenseBill = expenseBillRepository.findById(expenseBillId);
+        boolean res = true;
+        for(LineBill lineBill : expenseBill.getListLineBill()){
+            if(!lineBill.isValidated()){
+                res = false;
+                break;
+            }
+        }
+        return res;
+    }
+
+    public void setStateValidated(int expenseBillId){
+        ExpenseBill expenseBill = expenseBillRepository.findById(expenseBillId);
+        expenseBill.setState(BillStates.VALIDATED);
+        expenseBillRepository.save(expenseBill);
+    }
+
+    //TODO penser a faire une fonction qui gere le moment ou le colaborateur demande a valider sa note
 
 }
