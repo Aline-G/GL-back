f26825b615d53273afcb61400f616ffaf31722f3
guillaume-besse <38402818+guillaume-besse@users.noreply.github.com>
2022-01-25 17:14:59 +0100

    feature : possibility to ask for the validation of an advance

--- src/main/java/com/example/demo/service/AdvanceService.java
+++ src/main/java/com/example/demo/service/AdvanceService.java
@@ -1,8 +1,11 @@
 package com.example.demo.service;
 
 import com.example.demo.exception.AdvanceException;
+import com.example.demo.exception.ExpenseBillException;
 import com.example.demo.repository.AdvanceRepository;
 import com.example.demo.vo.Advance;
+import com.example.demo.vo.BillStates;
+import com.example.demo.vo.ExpenseBill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -27,5 +30,15 @@ public class AdvanceService {
 
     public List<Advance> getAdvanceList() {
         return StreamSupport.stream(this.advanceRepository.findAll().spliterator(), false).collect(Collectors.toList());
+    }
+
+    public Advance askForValidation(int id) throws AdvanceException {
+        if (!this.advanceRepository.existsById(id)) {
+            throw new AdvanceException("This advance does not exists", HttpStatus.CONFLICT);
+        }
+        Advance advance = advanceRepository.findById(id);
+        advance.setState(BillStates.WAITING);
+        advanceRepository.save(advance);
+        return advance;
     }
 }
